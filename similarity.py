456d0785fce98fd1198a68f997d886bfb3d5abb3
Johnny Clapham <71145206+johnnyclapham@users.noreply.github.com>
2022-12-11 22:44:25 -0500

    cleanup

--- similarity.py
+++ similarity.py
@@ -16,23 +16,9 @@ tensor_s2v = tf.io.read_file(
 
 tensor_bert = tf.convert_to_tensor(tensor_bert)
 tensor_s2v = tf.convert_to_tensor(tensor_s2v)
-# cos = torch.nn.CosineSimilarity(dim=0)
-
+cos = torch.nn.CosineSimilarity(dim=0)
 print(type(tensor_bert))
 print(type(tensor_s2v))
-# tensor_bert = tf.strings.to_number(tensor_bert, tf.int32)
-# tensor_s2v = tf.strings.to_number(tensor_s2v, tf.int32)
-# s = tf.keras.losses.cosine_similarity(tensor_bert,tensor_s2v)
-# print("Cosine Similarity:",s)
-
-# print(tensor_bert.dtype)
-# # tensor_bert = torch.tensor(tensor_bert, dtype=torch.float32)
-# tensor_bert.type(torch.int64)
-# print(tensor_bert.dtype)
-
-# cos_sim = cosine_similarity(tensor_bert,tensor_s2v)
-
-# s = tf.keras.losses.cosine_similarity(tensor_bert,tensor_bert)
-
+output = cos(tensor_bert, tensor_s2v)
 
 print('yam')
